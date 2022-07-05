c5928b65273c694f5cc9e471a5540c1ad68a9ddf
AlbertoFreije <freicartobe@gmail.com>
2022-07-05 10:39:43 +0200

    hjuh

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,6 +6,13 @@ pipeline {
         jdk 'jdk8' 
     }
     stages {
+        stage('chrome'){
+			
+			steps{
+               sh("wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \ && echo \"deb http://dl.google.com/linux/chrome/deb/ stable main\" >> /etc/apt/sources.list.d/google.list")
+               sh "apt-get update && apt-get -y install google-chrome-stable"
+		    }
+		}	
         stage('Maven'){
 			
 			steps{
--- src/test/java/pruebaSelenium/Test1.java
+++ src/test/java/pruebaSelenium/Test1.java
@@ -68,6 +68,7 @@ public class Test1 {
 	public void conectarPagina() {
 		
 		 System.setProperty("webdriver.chrome.driver","/var/jenkins_home/workspace/Selenium-Zap/chromedriver.exe");
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\alberto.freije\\OneDrive - Ricoh Europe PLC\\Desktop\\chromedriver_win32\\chromedriver.exe");
 		 driver = new ChromeDriver();
 		 String expectedTitle = "Welcome to the Simple Travel Agency!";
 	     //String actualTitle = "";
